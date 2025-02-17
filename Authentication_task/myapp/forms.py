08ac7c49bcede6e35e8230515f91bc6222254b8e
Gousiya10 <gousiyaaparadh1006@gmail.com>
2025-02-17 16:54:16 +0530

    Add files via upload

---
+++ Authentication_task/myapp/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+from .models import Item
+
+class ItemForm(ModelForm):
+    
+    class Meta:
+        model = Item
+        fields = ['name', 'description', 'price']
+        # fields = '__all__'
+        
+        
+    
